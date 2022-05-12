d3ffc99444a67dbf89be213de097179f9e78d2b3
Jared McKinney <jmckinney@microsoft.com>
2022-05-12 11:27:34 -0400

    added function to divide two numbers

---
+++ divide.java
@@ -0,0 +1,10 @@
+class divide{
+
+    public static void main(String[] args) {
+        System.out.println(divideNum(10,2));
+    }
+
+    public static int divideNum(int a, int b) {
+        return a / b;
+    }
+}
